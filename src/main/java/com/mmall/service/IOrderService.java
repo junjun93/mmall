04c222b33ef5ec1175b02fc553cfdb6267be5546
junjun <xidada2.0@gmail.com>
2018-02-22 17:20:01 +0800

    day13

--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,6 +1,10 @@
 package com.mmall.service;
 
+import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
+import com.mmall.vo.OrderVo;
+
+import java.util.Map;
 
 /**
  * @author junjun
@@ -8,5 +12,20 @@ import com.mmall.common.ServerResponse;
  **/
 public interface IOrderService {
 
-    public ServerResponse createOrder(Integer userId, Integer shippingId);
+    ServerResponse createOrder(Integer userId, Integer shippingId);
+    ServerResponse<String> cancel(Integer userId, Long orderNo);
+    ServerResponse getOrderCartProduct(Integer userId);
+    ServerResponse getOrderDetail(Integer userId, Long orderNo);
+    ServerResponse<PageInfo> getOrderList(Integer userId, Integer pageNum, Integer pageSize);
+
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+    ServerResponse aliCallback(Map<String,String> params);
+    ServerResponse<Boolean> queryOrderPayStatus(Integer userId, Long orderNo);
+
+    //backend
+    ServerResponse<PageInfo> manageList(Integer pageNum, Integer pageSize);
+    ServerResponse<OrderVo> manageDetail(Long orderNo);
+    ServerResponse<PageInfo> manageSearch(Long orderNo, Integer pageNum, Integer pageSize);
+    ServerResponse<String> manageSendGoods(Long orderNo);
+
 }
